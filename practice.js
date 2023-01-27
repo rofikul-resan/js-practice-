43fa9c30c28f0a4be9217efaaa87fcc4c39ad331
rofikul-resan <resan6231@gmail.com>
2023-01-27 20:52:33 +0600

    fail name change

---
+++ practice.js
@@ -0,0 +1,23 @@
+// var resan = 90;
+// var resad = 40;
+// var riyad = 31;
+// var siam = 55;
+
+// var student = resan;
+
+// if( student > 33 && student < 50){
+//     console.log('you gat D');
+// }else if (student >= 50 && student < 60 ){
+//     console.log('you gat C');
+// }else if (student >= 50 && student < 60 ){
+//     console.log('you gat C');
+// }else if (student >= 60 && student < 70 ){
+//     console.log('you gat B');
+// }else if (student >= 70 && student < 80 ){
+//     console.log('you gat A-');
+// }else if (student >= 80 && student <= 100 ){
+//     console.log('you gat A+');
+// }else{
+//     console.log('you gat F.. now everybody call yoû faltus');
+// }
+console.log( 6 = 6)
