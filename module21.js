0ee5b43fabbb4a1c8a2b7014d314c9246bf3def9
rofikul-resan <resan6231@gmail.com>
2023-02-02 23:51:38 +0600

    some changes

--- module21.js
+++ module21.js
@@ -19,3 +19,22 @@
 //   }
  
 //  console.log( sndLageValue(arr))
+
+
+
+// function fiboValue(arr, length){
+//     let newArray = arr
+//     for (let i = 2; i <= length; i++) {
+//         arr[i]= arr[i-1] + arr[i-2];   
+//     }
+//     console.log( newArray[newArray.length-1]);
+//     console.log( newArray[newArray.length-2]);
+//     return [ newArray[newArray.length-2], newArray[newArray.length-3], newArray  ]
+// }
+
+// let array= [0,1];
+
+// let result= fiboValue(array, 5);
+// console.log(result);
+
+
