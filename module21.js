3cc6ee69304e6f45c206cac3c5d8e1c8f7d5c2db
rofikul-resan <resan6231@gmail.com>
2023-02-02 23:08:51 +0600

    secend lagest value from an arrey

---
+++ module21.js
@@ -0,0 +1,21 @@
+// var arr= [1,2,34,67,74,67,89,34];
+//   function sndLageValue( value ){
+//     let a= 0;
+//     let b= 0;
+//     for(let i=0; i<value.length ; i++){
+//       if (a > value[i]){
+//         b = a ;
+//         a = value[i];
+//       }else if (a == value[i]) {
+//         continue
+//       } else {
+//         if (b < value[i]) {
+//           b = value[i]
+//         }
+        
+//       }
+//     }
+//     return b
+//   }
+ 
+//  console.log( sndLageValue(arr))
